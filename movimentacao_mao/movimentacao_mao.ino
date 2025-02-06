56da792000d16cc856e92a0a66a1f9f422b65c2d
rafacpovilla <rafacpovilla@gmail.com>
2025-02-06 10:24:45 -0300

    teste de rotacao

--- movimentacao_mao/movimentacao_mao.ino
+++ movimentacao_mao/movimentacao_mao.ino
@@ -56,24 +56,67 @@ void data() {
   }
 }
 
+int associaAngulo (int detectado) {
+  int angulo;
+
+  if(detectado == 0) {
+    angulo == 0;
+  }
+
+  else if(detectado == 1) {
+    angulo == 45;
+  }
+
+  else if (detectado == 2) {
+    angulo == 90;
+  }
+
+  else if (detectado == 3) {
+    angulo == 135;
+  }
+
+  else {
+    angulo == 180;
+  }
+
+  return angulo;
+}
+
 
 void loop() {
   data();
+  int angulo;
   
-  if (valoresRecebidos[0] == 1) { servoPolegar.write(180); }        //se a funcao data retornou 1 na posicao da string que representa o polegar, quer dizer que o dedo levanta (ou seja, manda o servo motor rodar 180 graus no sentido antihorario)
-  else { servoPolegar.write(0); }                                   //se retornou 0, quer dizer que o dedo abaixou (ou seja, manda o servo motor rodar 180 graus no sentido horario)
+  
+  angulo = associaAngulo(valoresRecebidos[0]);
+  servoPolegar.write(angulo);
 
-  if (valoresRecebidos[1] == 1) { servoIndicador.write(180); } 
-  else { servoIndicador.write(0); }
+  angulo = associaAngulo(valoresRecebidos[1]);
+  servoIndicador.write(angulo);
 
-  if (valoresRecebidos[2] == 1) { servoMeio.write(180); } 
-  else { servoMeio.write(0); }
+  angulo = associaAngulo(valoresRecebidos[2]);
+  servoMeio.write(angulo);
 
-  if (valoresRecebidos[3] == 1) { servoAnelar.write(180); } 
-  else { servoAnelar.write(0); }
+  angulo = associaAngulo(valoresRecebidos[3]);
+  servoAnelar.write(angulo);
 
-  if (valoresRecebidos[4] == 1) { servoMindinho.write(180); } 
-  else { servoMindinho.write(0); }
+  angulo = associaAngulo(valoresRecebidos[4]);
+  servoMindiinho.write(angulo);
+
+  // if (valoresRecebidos[0] == 1) { servoPolegar.write(180); }        //se a funcao data retornou 1 na posicao da string que representa o polegar, quer dizer que o dedo levanta (ou seja, manda o servo motor rodar 180 graus no sentido antihorario)
+  // else { servoPolegar.write(0); }                                   //se retornou 0, quer dizer que o dedo abaixou (ou seja, manda o servo motor rodar 180 graus no sentido horario)
+
+  // if (valoresRecebidos[1] == 1) { servoIndicador.write(180); } 
+  // else { servoIndicador.write(0); }
+
+  // if (valoresRecebidos[2] == 1) { servoMeio.write(180); } 
+  // else { servoMeio.write(0); }
+
+  // if (valoresRecebidos[3] == 1) { servoAnelar.write(180); } 
+  // else { servoAnelar.write(0); }
+
+  // if (valoresRecebidos[4] == 1) { servoMindinho.write(180); } 
+  // else { servoMindinho.write(0); }
 
 
 }
